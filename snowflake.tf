c298b146784a72872ba4f716313cddfdaf955aa0
soumik-dcsg <138025997+soumik-dcsg@users.noreply.github.com>
2023-07-07 13:53:36 +0530

    Update snowflake.tf

--- snowflake.tf
+++ snowflake.tf
@@ -1,39 +1,24 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "chanzuckerberg/snowflake"
-      version = "0.25.36"
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.59"
     }
   }
 }
 
 provider "snowflake" {
-
+  role  = "ACCOUNTADMIN"
   username   = "SCHEMA_CHANGE_TEST_USER"
-
   password   = "DicksSportingGoods2023#$"
-
   account    = "sl38675"
-
   warehouse  = "EDW_ENGINEER_WH_S"
-
   database   = "ADMIN_UTILS"
-
   schema     = "SCHEMACHANGE"
-
 }
 
-
 resource "snowflake_snowsql" "select_query" {
-
   name     = "select_query"
-
   query    = "select * from \"ADMIN_UTILS\".\"SCHEMACHANGE\".\"CHANGE_HISTORY\" limit 100"
-
-  database = "ADMIN_UTILS"
-
-  schema   = "SCHEMACHANGE"
-
-  role     = "DEV_SYSADMIN"
 
 }
