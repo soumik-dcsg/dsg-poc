a18b2289ce8e55cd8e6bb7ee444f08b39ce724fc
soumik-dcsg <138025997+soumik-dcsg@users.noreply.github.com>
2023-07-07 09:30:02 +0530

    Create snowflake.tf

---
+++ snowflake.tf
@@ -0,0 +1,30 @@
+provider "snowflake" {
+
+  username   = "SCHEMA_CHANGE_TEST_USER"
+
+  password   = "DicksSportingGoods2023#$"
+
+  account    = "sl38675"
+
+  warehouse  = "EDW_ENGINEER_WH_S"
+
+  database   = "ADMIN_UTILS"
+
+  schema     = "SCHEMACHANGE"
+
+}
+
+
+resource "snowflake_snowsql" "select_query" {
+
+  name     = "select_query"
+
+  query    = "select * from \"ADMIN_UTILS\".\"SCHEMACHANGE\".\"CHANGE_HISTORY\" limit 100"
+
+  database = "ADMIN_UTILS"
+
+  schema   = "SCHEMACHANGE"
+
+  role     = "DEV_SYSADMIN"
+
+}
